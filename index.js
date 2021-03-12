740134905d448ef87d2464ed35ef35eb3419fb97
season_studio <season_studio@outlook.com>
2021-03-12 14:53:40 +0800

    change export * as to export const = require

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export * from "./src/i18n";
 export * from "./src/domKits";
-export * as DomKits from "./src/domKits";
+export const DomKits = require("./src/domKits.js");
 export * from "./src/assert";
 export * from "./src/config";
 export * from "./src/tip";
